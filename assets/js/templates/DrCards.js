164783f67e24bf94f6724510732c13221fe3c0d7
Hiram Sanchez <hiram.sanchz@gmail.com>
2022-05-27 21:56:48 -0500

    Correcion de las rutas de imagenes del template DrCards.js

---
+++ assets/js/templates/DrCards.js
@@ -0,0 +1,42 @@
+//Constructor de tarjeta de doctor
+DoctorCards = function(imgLink,doctorName,doctorCity,doctorRating,doctorEspecialidad,doctorDescripcion){
+
+    //*Calificacion del doctor en estrellas
+    let parteDecimal=doctorRating%1;
+    let star="<img src=\"./assets/img/Star.svg\">";
+    //Muestra las estrellas completas de la calificacion segun el número de calificacion.
+    if (doctorRating>0&&doctorRating<=1.75)
+        {StarRating= star;}
+        else if (doctorRating>1.75&&doctorRating<=2.75)
+            {StarRating=star+star;}
+        else if (doctorRating>2.75&&doctorRating<=3.75)
+            {StarRating=star+star+star;}
+        else if (doctorRating>3.75&&doctorRating<=4.75)
+            {StarRating=star+star+star+star;}
+        else if (doctorRating>4.75&&doctorRating<=5)
+            {StarRating=star+star+star+star+star;}
+    else{StarRating="";}
+    //Muestra una media estrella si la parte decimal del numero es mayor a 0.25, y menor a 0.75 ya que en ese punto se creará otra estrella completa por promedio.
+    if(parteDecimal>=0.25&&parteDecimal<=0.75){
+        StarRating+="<img src=\"./assets/img/halfStar.svg\" width=\"24px\" height=\"20px\">";}
+
+    //*Construccion de la tarjeta
+    cardCreate.innerHTML=`<div class="card" style="width: 18rem;">
+    <div class="imgContainer">
+    <img src=` + imgLink + `class="card-img-top" alt="...">
+    <div class="drData">
+        <div class="drName">` + doctorName + `</div>
+        <div class="drCity">` + doctorCity + `</div>
+        <div id="starRate">` + StarRating + `</div>
+    </div>
+    </div>
+    <div class="card-body">
+    <p class="especialidad">` + doctorEspecialidad + `</p>
+    <p class="descripcion">` + doctorDescripcion + `</p>
+    <a href="#" class="btnAgendarCita">Agendar Cita</a>
+    </div>
+    </div>`;
+}
+
+const cardCreate = document.querySelector('#cardCreate');
+cardCreate.appendChild(DoctorCards(`"./assets/img/docotrimg.png"`,"Shaun Murphy","Guadalajara","4.7","Cardiólogo","Médico Internista con especialidad en Cardiología."));
